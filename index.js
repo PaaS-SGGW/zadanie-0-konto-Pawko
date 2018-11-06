79be3c54031b721b9aa2a8dd243cdb65a16d9e29
Magda Sidor <magda_si@wp.pl>
2018-11-06 18:31:15 +0100

    init db

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM employees');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/index', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
